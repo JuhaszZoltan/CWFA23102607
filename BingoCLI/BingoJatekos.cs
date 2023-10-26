b51d4feb90943e9e56f5752627c0312f4f928519
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-10-26 14:52:06 +0200

    Add project files.

---
+++ BingoCLI/BingoJatekos.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingoCLI
+{
+    internal class BingoJatekos
+    {
+        public string Nev { get; private set; }
+        public int[,] Kartya { get; private set; }
+        private bool[,] Jelolesek { get; set; } = new bool[5, 5];
+
+        public void SorsoltSzamotJelol(int sorsoltSzam)
+        {
+            for (int s = 0; s < 5; s++)
+            {
+                for (int o = 0; o < 5; o++)
+                {
+                    if (Kartya[s, o] == sorsoltSzam)
+                    {
+                        Jelolesek[s, o] = true;
+                    }
+                }
+            }
+        }
+
+        public bool BingoEll
+        {
+            get
+            {
+                //sorok
+                for (int s = 0; s < 5; s++)
+                {
+                    int talalatS = 0;
+                    for (int o = 0; o < 5; o++)
+                    {
+                        if (Jelolesek[s, o]) talalatS++;
+                    }
+                    if (talalatS == 5) return true;
+                }
+
+                //oszlopok
+                for (int o = 0; o < 5; o++)
+                {
+                    int talalatO = 0;
+                    for (int s = 0; s < 5; s++)
+                    {
+                        if (Jelolesek[s, o]) talalatO++;
+                    }
+                    if (talalatO == 5) return true;
+                }
+
+                //egyik atlo:
+                int talalatA1 = 0;
+                for (int c = 0; c < 5; c++)
+                {
+                    if (Jelolesek[c, c]) talalatA1++;
+                }
+                if (talalatA1 == 5) return true;
+
+                //masik atlo
+                int talalatA2 = 0;
+                for (int c = 0; c < 5; c++)
+                {
+                    if (Jelolesek[c, 4 - c]) talalatA2++;
+                }
+                if (talalatA2 == 5) return true;
+
+                //egyébként
+                return false;
+            }
+        }
+
+        public BingoJatekos(string nev, int[,] kartya)
+        {
+            Nev = nev;
+            Kartya = kartya;
+            Jelolesek[2, 2] = true;
+        }
+    }
+}
---
+++ BingoCLI/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BingoCLI
+{
+    internal class Program
+    {
+        static List<BingoJatekos> jatekosok;
+
+        static void Main()
+        {
+            jatekosok = InitJatekosok(@"..\..\src");
+
+            Console.WriteLine($"4. feladat: játékosok száma: {jatekosok.Count}");
+
+
+            Console.ReadKey();
+        }
+
+        private static List<BingoJatekos> InitJatekosok(string path)
+        {
+            var lista = new List<BingoJatekos>();
+            string[] fileok = Directory.GetFiles(path);
+            foreach (var file in fileok)
+            {
+                if (file.Contains(".txt"))
+                {
+                    using (var sr = new StreamReader(file))
+                    {
+                        string nev = file.Split('\\')[3].Split('.')[0];
+                        int[,] kartya = new int[5, 5];
+                        int si = 0;
+                        while (!sr.EndOfStream)
+                        {
+                            string[] sor = sr.ReadLine().Split(';');
+                            int oi = 0;
+                            foreach (var szam in sor)
+                            {
+                                if (szam != "X") kartya[si, oi] = int.Parse(szam);
+                                oi++;
+                            }
+                            si++;
+                        }
+                        lista.Add(new BingoJatekos(nev, kartya));
+                    }
+                }
+            }
+            return lista;
+        }
+    }
+}
